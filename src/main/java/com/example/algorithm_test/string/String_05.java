7bbed34d2c62eefa14e44069b4c274c4c8cefd15
hajune <buea486@gmail.com>
2023-08-31 17:21:04 +0900

    회문 문제 다른 방법 풀이

--- src/main/java/com/example/algorithm_test/string/String_05.java
+++ src/main/java/com/example/algorithm_test/string/String_05.java
@@ -20,15 +20,12 @@ public class String_05 {
 
     public String solution(String str) {
         String answer = "";
-
-        for (int i = 0; i <str.length()/2; i++) {
-            if(str.charAt(i) != str.charAt(str.length()-i-1)) {
-                return "NO";
-            } else {
-                answer = "YES";
-            }
+        String reversStr = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(reversStr)) {
+            answer = "YES";
+        } else {
+            answer = "NO";
         }
-
         return answer;
     }
 
