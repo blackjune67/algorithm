b61f998359fb97b694b513754099a4d1ac4937c4
hajune <buea486@gmail.com>
2023-08-31 16:37:00 +0900

    BAEKJOON alogrithm 10171

---
+++ src/main/java/com/example/algorithm_test/string/String_04.java
@@ -0,0 +1,26 @@
+package com.example.algorithm_test.string;
+
+import java.util.Scanner;
+
+public class String_04 {
+    // ! 문제
+    // * //
+
+    // ! 입력
+    // * //
+
+    // ! 출력
+    // *
+
+    public String solution(String str) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
