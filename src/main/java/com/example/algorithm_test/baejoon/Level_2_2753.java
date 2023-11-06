0e5294d58cc617699a3f5192faa672308b9c29c3
hajune <buea486@gmail.com>
2023-11-07 00:13:35 +0900

    수정

--- src/main/java/com/example/algorithm_test/baejoon/Level_2_2753.java
+++ src/main/java/com/example/algorithm_test/baejoon/Level_2_2753.java
@@ -6,7 +6,8 @@ public class Level_2_2753 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int i = scanner.nextInt();
-        // * 4의 배수
+        // * 4의 배수이면서 100의 배수가 아니면 윤년
+        // * 1999년은 100의 배수, 400의 배수는 아니기 때문에 윤년
         if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
             System.out.println(1);
         } else {
